ec04d06da1223870d6b66a3e2766b2fe9391b7e4
hehaha <1050311695@qq.com>
2018-06-21 11:14:24 +0800

    Initial Commit

---
+++ Leetcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leetcode
+//
+//  Created by wecash on 2018/6/21.
+//  Copyright © 2018年 wecash. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
